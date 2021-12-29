95bb0af840f592e4e6f0f831f7d552e2710d82c8
Brandon <williamsb600@gmail.com>
2021-12-29 14:01:05 -0800

    Use fetch API to retrieve movie data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 import MovieModel from "./models/movie";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
-const dummyMovies: MovieModel[] = [
-  {
-    id: 1,
-    title: "Some Dummy Movie",
-    openingText: "This is the opening text of the movie",
-    releaseDate: "2021-05-18",
-  },
-  {
-    id: 2,
-    title: "Some Dummy Movie 2",
-    openingText: "This is the second opening text of the movie",
-    releaseDate: "2021-05-19",
-  },
-];
+const INITIAL_MOVIES: MovieModel[] = [];
 
-function App() {
+const STAR_WARS_API_URL = "https://swapi.dev/api/films/";
+
+const App = () => {
+  const [movies, setMovies] = useState(INITIAL_MOVIES);
+
+  async function fetchMovies() {
+    try {
+      const jsonResponse = await fetch(STAR_WARS_API_URL);
+      const dataObj = await jsonResponse.json();
+      setMovies(convertJSONMoviesToMovieModels(dataObj.results));
+    }catch(error) {
+      //In production we would do something more user friendly than log error
+      console.log(error)
+    }
+  }
+
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMovies}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        <MoviesList movies={movies} />
       </section>
     </React.Fragment>
   );
+};
+
+function convertJSONMoviesToMovieModels(jsonMovies: any[]): MovieModel[] {
+  return jsonMovies.map((movieObj) => {
+    return new MovieModel(
+      Math.random(),
+      movieObj.title,
+      movieObj.opening_crawl,
+      movieObj.release_date
+    );
+  });
 }
 
 export default App;
