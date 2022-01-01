d036d487986147da72b9ffaae53ea4e096982d7c
Brandon <williamsb600@gmail.com>
2021-12-31 20:02:50 -0800

    Add form validation

--- src/components/AddMovieForm.tsx
+++ src/components/AddMovieForm.tsx
@@ -14,6 +14,9 @@ const DEFAULT_FORM_STATE = {
   title: "",
   text: "",
   releaseDate: "",
+  isValid: false,
+  wasTouched: false,
+  error: "",
 };
 
 const AddMovieForm: React.FC<AddMovieFormProps> = (props) => {
@@ -28,10 +31,42 @@ const AddMovieForm: React.FC<AddMovieFormProps> = (props) => {
     dispatchForm({ type: event.target.id, value: event.target.value });
   }
 
+  function titleOnBlur(event: React.FocusEvent<HTMLInputElement>) {
+    let errorMessage = "";
+    let isValid = true;
+    if (formState.title.trim().length === 0) {
+      errorMessage = "Title is not valid";
+      isValid = false;
+    }
+    dispatchForm({ type: "validate", error: errorMessage, isValid: isValid });
+  }
+
+  function dateOnBlur(event: React.FocusEvent<HTMLInputElement>) {
+    //TODO: Fix date validation 
+    const isDate = !isNaN(new Date(formState.releaseDate).getDate());
+    let errorMessage = "";
+    let isValid = true;
+    if (!isDate) {
+      errorMessage =
+        "The date is not valid. Please format in date format YYYY-MM-DD";
+      isValid = false;
+    }
+    dispatchForm({ type: "validate", error: errorMessage, isValid: isValid });
+  }
+
+  function textOnBlur(event: React.FocusEvent<HTMLTextAreaElement>) {
+    let errorMessage = "";
+    let isValid = true;
+    if (formState.text.trim().length === 0) {
+      errorMessage = "Text area is not valid";
+      isValid = false;
+    }
+    dispatchForm({ type: "validate", error: errorMessage, isValid: isValid });
+  }
+
   function submitHandler(event: React.FormEvent) {
     event.preventDefault();
 
-    //Need some sort of input validation
     const newMovie = new MovieModel(
       Math.random(),
       formState.title,
@@ -53,31 +88,37 @@ const AddMovieForm: React.FC<AddMovieFormProps> = (props) => {
   }
 
   return (
-    <form onSubmit={submitHandler} className={classes.control}>
-      <label htmlFor="title">Title</label>
-      <input
-        id="title"
-        type="text"
-        value={formState.title}
-        onChange={inputOnChange}
-      ></input>
+    <React.Fragment>
+      <form onSubmit={submitHandler} className={classes.control}>
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          type="text"
+          value={formState.title}
+          onChange={inputOnChange}
+          onBlur={titleOnBlur}
+        ></input>
 
-      <label htmlFor="text">Opening Text</label>
-      <textarea
-        id="text"
-        value={formState.text}
-        onChange={textAreaOnChange}
-      ></textarea>
+        <label htmlFor="text">Opening Text</label>
+        <textarea
+          id="text"
+          value={formState.text}
+          onChange={textAreaOnChange}
+          onBlur={textOnBlur}
+        ></textarea>
 
-      <label htmlFor="releaseDate">Release Date</label>
-      <input
-        id="releaseDate"
-        type="text"
-        value={formState.releaseDate}
-        onChange={inputOnChange}
-      ></input>
-      <button type="submit">Add Movie</button>
-    </form>
+        <label htmlFor="releaseDate">Release Date</label>
+        <input
+          id="releaseDate"
+          type="text"
+          value={formState.releaseDate}
+          onChange={inputOnChange}
+          onBlur={dateOnBlur}
+        ></input>
+        <button type="submit" disabled={!formState.isValid}>Add Movie</button>
+      </form>
+      {formState.wasTouched && !formState.isValid && <p>{formState.error}</p>}
+    </React.Fragment>
   );
 };
 
@@ -99,6 +140,14 @@ function formReducer(prevState: any, action: any) {
       return {
         ...prevState,
         releaseDate: action.value,
+      };
+
+    case "validate":
+      return {
+        ...prevState,
+        isValid: action.isValid,
+        wasTouched: true,
+        error: action.error,
       };
 
     default:
